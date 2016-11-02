0690452782b69c26226b791e765bb7537db26b93
Paula Lee <pdlee877@gmail.com>
2016-11-02 09:37:49 -0700

    complete step 6 and part of step 4

--- favorite-toys/db/migrate/20161102161630_create_pets.rb
+++ favorite-toys/db/migrate/20161102161630_create_pets.rb
@@ -4,7 +4,7 @@ class CreatePets < ActiveRecord::Migration[5.0]
       t.string :name, null: false
       t.string :breed, null: false
       t.integer :age, null: false
-      t.boolean :cute, null: false
+      t.boolean :cute
 
       t.timestamps
     end
